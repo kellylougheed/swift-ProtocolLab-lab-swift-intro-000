86cc088d426e6f9048942155fa84e933281ba24f
Kelly Lougheed <kellylougheed@gmail.com>
2017-04-30 22:36:22 -0700

    Add answers

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -20,7 +20,21 @@ class BankVault {
     
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
 
 
-
-
+extension BankVault: ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        if password.isEmpty || password.count > 10 {
+            return false
+        }
+        for (index, num) in password.enumerated() {
+            if index % 2 == 0 && num % 2 != 0 {
+                return false
+            }
+        }
+        return true
+    }
+}
